b95c53f23812a232b761c5fb8c5d442c7380e29e
josimar.melo@gertec.com.br <josimarferreira922@yahoo.com.br>
2024-03-12 16:55:44 -0300

    [feature] Utiliza variável de ambiente

--- cypress/integration/examples/Test9_Hooks.js
+++ cypress/integration/examples/Test9_Hooks.js
@@ -13,7 +13,9 @@ describe("Hooks test", function () {
   });
 
   it("Demo test", function () {
-    cy.visit("https://rahulshettyacademy.com/angularpractice/");
+    //Podemos usar o comando abaixo para sobrescrever uma propriedade específica para um único teste
+    Cypress.config("defaultCommandTimeout", 8000);
+    cy.visit(Cypress.env("url") + "/angularpractice/");
 
     const homePage = new HomePage();
     const productPage = new ProductPage();
@@ -43,7 +45,36 @@ describe("Hooks test", function () {
     this.data.productName.forEach(function (element) {
       cy.selectProduct(element);
     });
-
     productPage.getCheckoutButton().click();
+
+    var sum = 0;
+    cy.get("tr td:nth-child(4) strong")
+      .each(($el, index, list) => {
+        const amount = $el.text();
+        let res = amount.split(" ");
+        res = res[1].trim();
+        sum += Number(res);
+      })
+      .then(function () {
+        cy.log(sum);
+      });
+    cy.get("h3 strong").then(function (element) {
+      const amount = element.text();
+      let res = amount.split(" ");
+      let total = res[1].trim();
+      expect(Number(total)).to.equal(sum);
+    });
+
+    // cy.pause();
+
+    cy.contains("Checkout").click();
+    cy.get("#country").type("India");
+    cy.get(".suggestions > ul > li > a").click();
+    cy.get("#checkbox2").click({ force: true });
+    cy.get('input[type="submit"').click();
+    cy.get(".alert").then(function (element) {
+      const actualText = element.text();
+      expect(actualText.includes("Success")).to.be.true;
+    });
   });
 });
