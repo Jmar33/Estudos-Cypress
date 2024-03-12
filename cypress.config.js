f2e4d2b271a5f11908c6ecd30379c40d06035cec
josimar.melo@gertec.com.br <josimarferreira922@yahoo.com.br>
2024-03-12 16:55:06 -0300

    [feature] Conecta o Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 6000,
+  env: {
+    url: "https://rahulshettyacademy.com",
+  },
+  retries: {
+    runMode: 1, //Testa nosso app novamente em caso de falhas
+  },
+  projectId: "ci1gop",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
